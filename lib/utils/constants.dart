6f3a872826a57608918bd9088df1ab3f5165c8f7
Darioantonio20 <antoniodario10@hotmail.com>
2025-06-20 00:07:15 -0600

    ADD skeleton of the main proyects in appointment

---
+++ lib/utils/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// Paleta de colores
+const Color primaryColor = Color(0xFF1976D2); // Color principal, azul médico
+const Color accentColor = Color(0xFF90CAF9); // Color de acento, azul claro
+const Color secondaryColor =
+    Color(0xFF1565C0); // Color secundario, azul más oscuro
+const Color lightBackgroundColor =
+    Color(0xFFE3F2FD); // Fondo claro, azul muy claro
+const Color patientFirstAppointmentColor =
+    Color(0xFF64B5F6); // Citas por primera vez, azul intermedio
+const Color patientSubsequentAppointmentColor =
+    Color(0xFFB3E5FC); // Citas subsecuentes, celeste
+const Color missedAppointmentColor = Colors.red; // Cita no asistida
+const Color attendedAppointmentColor = Colors.green; // Cita asistida
+const Color pendingAppointmentColor = Colors.orange; // Cita pendiente
